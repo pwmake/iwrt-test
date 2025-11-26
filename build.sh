82904a2ee45770c0d4b3dc5baa0239fd123944bb
Jeff Chan <46584359+pwmake@users.noreply.github.com>
2025-11-26 01:31:39 +0000

    introduce cf settings

--- build.sh
+++ build.sh
@@ -1,3 +1,77 @@
-cd /home/build/immortalwrt && \
-              make info && \
-              make image PROFILE="generic" PACKAGES="luci-theme-argon luci-app-openclash luci-app-nps"
+#!/usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# @file
+# Builds a Hugo site hosted on a Cloudflare Worker.
+#
+# The Cloudflare Worker automatically installs Node.js dependencies.
+#------------------------------------------------------------------------------
+
+main() {
+
+  DART_SASS_VERSION=1.93.2
+  GO_VERSION=1.25.3
+  HUGO_VERSION=0.152.2
+  NODE_VERSION=22.20.0
+
+  export TZ=Europe/Oslo
+
+  # Install Dart Sass
+  echo "Installing Dart Sass ${DART_SASS_VERSION}..."
+  curl -sLJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  tar -C "${HOME}/.local" -xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  rm "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  export PATH="${HOME}/.local/dart-sass:${PATH}"
+
+  # Install Go
+  echo "Installing Go ${GO_VERSION}..."
+  curl -sLJO "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+  tar -C "${HOME}/.local" -xf "go${GO_VERSION}.linux-amd64.tar.gz"
+  rm "go${GO_VERSION}.linux-amd64.tar.gz"
+  export PATH="${HOME}/.local/go/bin:${PATH}"
+
+  # Install Hugo
+  echo "Installing Hugo ${HUGO_VERSION}..."
+  curl -sLJO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  mkdir "${HOME}/.local/hugo"
+  tar -C "${HOME}/.local/hugo" -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  rm "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  export PATH="${HOME}/.local/hugo:${PATH}"
+
+  # Install Node.js
+  echo "Installing Node.js ${NODE_VERSION}..."
+  curl -sLJO "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz"
+  tar -C "${HOME}/.local" -xf "node-v${NODE_VERSION}-linux-x64.tar.xz"
+  rm "node-v${NODE_VERSION}-linux-x64.tar.xz"
+  export PATH="${HOME}/.local/node-v${NODE_VERSION}-linux-x64/bin:${PATH}"
+
+  # Verify installations
+  echo "Verifying installations..."
+  echo Dart Sass: "$(sass --version)"
+  echo Go: "$(go version)"
+  echo Hugo: "$(hugo version)"
+  echo Node.js: "$(node --version)"
+
+  # Configure Git
+  echo "Configuring Git..."
+  git config core.quotepath false
+  if [ "$(git rev-parse --is-shallow-repository)" = "true" ]; then
+    git fetch --unshallow
+  fi
+
+  echo "start to retrieve hugo theme even" 
+  git clone https://github.com/olOwOlo/hugo-theme-even themes/even
+
+  echo "start to enhance layout to support mermaid"
+    mkdir -pv themes/even/layouts/_markup && \
+        cp -v ./layout_enhancement/render-codeblock-mermaid.html ./themes/even/layouts/_markup/render-codeblock-mermaid.html
+    cat ./layout_enhancement/scripts.html >> themes/even/layouts/partials/scripts.html
+
+  # Build the site
+  echo "Building the site..."
+  hugo --gc --minify
+
+}
+
+set -euo pipefail
+main "$@"
---
+++ build_iwrt.sh
@@ -0,0 +1,3 @@
+cd /home/build/immortalwrt && \
+              make info && \
+              make image PROFILE="generic" PACKAGES="luci-theme-argon luci-app-openclash luci-app-nps"
